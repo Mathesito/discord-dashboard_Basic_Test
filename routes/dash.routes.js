245e4fffc985be3257c0d5264638853c817f06a0
The-vato <Florandy303@gmail.com>
2023-06-15 21:56:11 -0500

    Change

--- routes/dash.routes.js
+++ routes/dash.routes.js
@@ -3,22 +3,53 @@ const { auth } = require("../util/middleware/auth");
 const router = Router();
 
 router.get("/dash", auth, (req, res) => {
-  res.json({
+
+  let servidores = [];
+  let guilds = req.user.guilds.filter(p => (p.permissions & 8) === 8)
+
+  for (const key in guilds) {
+    if (req.BotClient.guilds.cache.get(guilds[key].id)) {
+      servidores.push({
+        esta: true,
+        id: req.BotClient.guilds.cache.get(guilds[key].id).id,
+        name: req.BotClient.guilds.cache.get(guilds[key].id).name,
+        icon: req.BotClient.guilds.cache.get(guilds[key].id).icon
+      })
+    } else {
+      servidores.push({
+        esta: false,
+        id: guilds[key].id,
+        name: guilds[key].name,
+        icon: guilds[key].icon
+    })
+  }
+}
+
+
+  res.render("dash", {
     user: req.user,
-    bot: req.BotClient,
+    servidores
   });
 });
 
 router.get("/dash/:id", auth, (req, res) => {
   let id = req.params.id;
   let servidor = req.BotClient.guilds.cache.get(id);
-  let canales = servidor.channels.cache;
+  let emoji = JSON.stringify(servidor.emojis.cache);
+  // res.json({
+  //   servidor,
+  //   canales: servidor.channels.cache,
+  //   roles: servidor.roles.cache,
+  //   emojis: JSON.parse(emoji),
+  // })
 
-  res.render("dash", {
-    user: req.user,
+ res.render("data", {
+   user: req.user,
     servidor,
-    canales,
-  });
+    canales: servidor.channels.cache.filter(ch => ch.type === "text").map(ch => ({name: ch.name, id: ch.id})),
+    roles: servidor.roles.cache,
+    emojis: JSON.parse(emoji),
+  })
 });
 
 module.exports = router;
